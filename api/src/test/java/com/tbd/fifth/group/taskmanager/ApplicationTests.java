0627c6bc55a995485c22000be9d65cc376332184
Fernando Solis Paz <fernando.solis.p@usach.cl>
2024-04-23 18:49:40 -0400

    api initialized

---
+++ api/src/test/java/com/tbd/fifth/group/taskmanager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbd.fifth.group.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
