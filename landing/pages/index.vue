c421030530c9dd0582fe24e0c09bfe8f2a4a1dce
Fabián Lizama <fabian.lizama@usach.cl>
2024-04-29 06:14:47 -0400

    landing notifications and other functionalities implemented

--- landing/pages/index.vue
+++ landing/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { format } from 'date-fns'
+import { format, isWithinInterval, addDays  } from 'date-fns'
 
 const colorMode = useColorMode();
 
@@ -27,21 +27,21 @@ if (!tokenCookie.value) {
 const tasks = ref([
     {
         task_id: 1,
-        title: 'Tarea 1',
+        title: 'Entregar Control',
         description: 'Descripción de la tarea 1',
         state: 'Pending',
         date_of_expire: '2024-10-10',
         isOpen: false,
-        dateObj: new Date('2024-10-10T00:00:00')
+        dateObj: new Date('2024-04-30T00:00:00')
     },
     {
         task_id: 2,
-        title: 'Tarea 2',
+        title: 'Conectar backend',
         description: 'Descripción de la tarea 2',
         state: 'Pending',
         date_of_expire: '2024-10-11',
         isOpen: false,
-        dateObj: new Date('2024-10-11T00:00:00')
+        dateObj: new Date('2024-04-30T00:00:00')
     }
 ]);
 
@@ -78,14 +78,68 @@ function deleteTask(task) {
     tasks.value = tasks.value.filter(t => t.task_id !== task.task_id);
 }
 
+// Paginación y filtrado
+const currentPage = ref(1);
+const tasksPerPage = ref(5);
+const filter = ref('');
+const filterStatus = ref('All');
+
+const filteredTasks = computed(() => {
+  return tasks.value.filter(task => {
+    if (filterStatus.value === 'All') {
+      return task.title.toLowerCase().includes(filter.value.toLowerCase());
+    } else {
+      return task.title.toLowerCase().includes(filter.value.toLowerCase()) && task.state === filterStatus.value;
+    }
+  }).slice((currentPage.value - 1) * tasksPerPage.value, currentPage.value * tasksPerPage.value);
+});
+
+const filteredTaskCount = computed(() => {
+  return tasks.value.filter(task =>
+    task.title.toLowerCase().includes(filter.value.toLowerCase())
+  ).length;
+});
+
+function toggleTaskState(task) {
+    task.state = task.state === 'Completed' ? 'Pending' : 'Completed';
+}
+
+// Notificaciones
+const toast = useToast();
+function checkTasksExpiringSoon() {
+    const today = new Date();
+    const twoDaysFromNow = addDays(today, 2);
+
+    const expiringTasks = tasks.value.filter(task =>
+        isWithinInterval(task.dateObj, { start: today, end: twoDaysFromNow })
+    );
+    if (expiringTasks.length > 0) {
+        expiringTasks.forEach(task => {
+            toast.add({ title:`La tarea ${task.title} expirará pronto`, color:'green', timeout:0, icon:'i-heroicons-bell-alert-16-solid'});
+        });
+    }
+}
+
+onMounted(() => {
+    checkTasksExpiringSoon();
+});
+
 </script>
 
 <template>
     <div class="flex justify-center items-center content-below-appbar">
         <UCard class="w-screen mx-52 min-w-fit ring-0">
             <h1 class="font-bold text-primary text-3xl mx-10 py-4 mb-4">Tareas</h1>
+            <div class="flex items-center justify-between">
+                <UInput v-model="filter" class="mx-10 my-4" size="md" icon="i-heroicons-magnifying-glass" color="primary" placeholder="Buscar una tarea"/>
+                <div class="mx-10 my-4">
+                    <UButton @click="filterStatus = 'All'" :variant="filterStatus === 'All' ? 'solid' : 'ghost'">Todas</UButton>
+                    <UButton class="m-4" @click="filterStatus = 'Completed'" :variant="filterStatus === 'Completed' ? 'solid' : 'ghost'">Completadas</UButton>
+                    <UButton @click="filterStatus = 'Pending'" :variant="filterStatus === 'Pending' ? 'solid' : 'ghost'">Pendientes</UButton>
+                </div>
+            </div>
 
-            <div v-if="taskLength > 0" v-for="task in tasks" :key="task.task_id" class="flex px-10 items-center">
+            <div v-if="taskLength > 0" v-for="task in filteredTasks" :key="task.task_id" class="flex px-10 items-center">
                 <UModal v-model="task.isOpen">
                     <UCard>
                         <UForm>
@@ -115,10 +169,13 @@ function deleteTask(task) {
                 <UCard class="w-full my-2">
                     <div class="flex items-center justify-between">
                         <div class="flex items-center">
-                            <UCheckbox v-model="task.state" class="text-primary py-2"/>
+                            <UCheckbox :checked="task.state === 'Completed'" @change="toggleTaskState(task)" class="text-primary py-2"/>
                             <p class="text-primary font-medium px-4 hover:text-blue-500 cursor-pointer" @click="task.isOpen = true">{{ task.title }}</p>
                         </div>
-                        <UButton class="delete" variant="link" color="red" icon="i-heroicons-trash" @click="deleteTask(task)" />
+                        <div class="flex items-center">
+                            <p class="text-primary font-medium mr-2">{{ format(task.dateObj, 'd MMM, yyy') }}</p>
+                            <UButton class="delete" variant="link" color="red" icon="i-heroicons-trash" @click="deleteTask(task)" />
+                        </div>
                     </div>
                     </UCard>
             </div>
@@ -127,6 +184,7 @@ function deleteTask(task) {
                 <p class="text-primary mx-10 py-2 font-medium">Intenta añadiendo una nueva tarea</p>
                 <UIcon name="i-heroicons-arrow-down" class="w-8 h-8 text-primary mt-2"/>
             </div>
+            <UPagination :total="filteredTaskCount" :per-page="filteredTaskCount" v-model="currentPage" class="my-4 mx-10 text-center"/>
             <div class="flex items-center mx-10 py-4 mt-4 justify-between">
 
                 <UInput v-model="title" size="xl" class="w-full" icon="i-heroicons-plus-circle" color="primary" placeholder="Agregar una tarea" :ui="{ icon: { trailing: { pointer: '' } } }">
